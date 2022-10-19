a67c8a6737edeefd71fc34b25ff65e57a50931d8
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-10-19 11:41:41 +0600

    country added

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,18 +1,68 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const Problem2 = () => {
+    const [contact, setContact] = useState({});
+
+    useEffect(() => {
+        fetch('https://contact.mediusware.com/api-doc/?format=openapi')
+            .then(res => res.json())
+            .then(data => {
+                setContact(data)
+            })
+    }, [])
+
 
     return (
 
         <div className="container">
             <div className="row justify-content-center mt-5">
                 <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
+
                 <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        All Contact
+                    </button>
+
+                    {/* <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button> */}
+                    <button type="button" className="btn btn-lg btn-outline-warning" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+                        US Contacts
+                    </button>
                 </div>
-                
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">All Counties</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                               <h2>E-mail: {contact?.info?.contact?.email}</h2>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                US Contacts
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
